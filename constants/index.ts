8beb4600d8b18c50b6af7f5f404dc446be3cbb4e
Taher Max <hathitaher73@gmail.com>
2024-09-13 19:32:30 +0530

    doctors to doctor

--- constants/index.ts
+++ constants/index.ts
@@ -43,40 +43,40 @@ export const IdentificationTypes = [
 export const Doctors = [
   {
     image: "/assets/images/dr-green.png",
-    name: "John Green",
+    name: "Neeraj Teotia",
   },
-  {
-    image: "/assets/images/dr-cameron.png",
-    name: "Leila Cameron",
-  },
-  {
-    image: "/assets/images/dr-livingston.png",
-    name: "David Livingston",
-  },
-  {
-    image: "/assets/images/dr-peter.png",
-    name: "Evan Peter",
-  },
-  {
-    image: "/assets/images/dr-powell.png",
-    name: "Jane Powell",
-  },
-  {
-    image: "/assets/images/dr-remirez.png",
-    name: "Alex Ramirez",
-  },
-  {
-    image: "/assets/images/dr-lee.png",
-    name: "Jasmine Lee",
-  },
-  {
-    image: "/assets/images/dr-cruz.png",
-    name: "Alyana Cruz",
-  },
-  {
-    image: "/assets/images/dr-sharma.png",
-    name: "Hardik Sharma",
-  },
+  // {
+  //   image: "/assets/images/dr-cameron.png",
+  //   name: "Leila Cameron",
+  // },
+  // {
+  //   image: "/assets/images/dr-livingston.png",
+  //   name: "David Livingston",
+  // },
+  // {
+  //   image: "/assets/images/dr-peter.png",
+  //   name: "Evan Peter",
+  // },
+  // {
+  //   image: "/assets/images/dr-powell.png",
+  //   name: "Jane Powell",
+  // },
+  // {
+  //   image: "/assets/images/dr-remirez.png",
+  //   name: "Alex Ramirez",
+  // },
+  // {
+  //   image: "/assets/images/dr-lee.png",
+  //   name: "Jasmine Lee",
+  // },
+  // {
+  //   image: "/assets/images/dr-cruz.png",
+  //   name: "Alyana Cruz",
+  // },
+  // {
+  //   image: "/assets/images/dr-sharma.png",
+  //   name: "Hardik Sharma",
+  // },
 ];
 
 export const StatusIcon = {
